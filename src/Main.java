34d5680c2fa8c1422eeb18ba7f4c50b87105036b
omurbekavalbekov <omurbekavalbekov9@gmail.com>
2024-02-27 14:53:55 +0800

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        for (int i = 100; i <=1000; i++) {
+            if (i % 2 ==1 && i % 5 ==0)
+                System.out.println(i);
+        }
     }
 }
